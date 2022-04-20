424e93ea5b7ec44c088ea42d019a8a2c73799b85
elliedotjpg <79950128+elliedotjpg@users.noreply.github.com>
2022-04-20 22:03:30 +0800

    🐛 Fixed CameraFollowPlayer script issue

--- Wanderfell/Assets/Scripts/CameraFollowPlayer.cs
+++ Wanderfell/Assets/Scripts/CameraFollowPlayer.cs
@@ -9,11 +9,15 @@ public class CameraFollowPlayer : MonoBehaviour
     public Transform target;
     public float yOffset = 1f;
 
+    void Start()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y + yOffset);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
