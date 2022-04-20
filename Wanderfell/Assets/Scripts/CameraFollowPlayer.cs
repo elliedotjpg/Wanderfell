a966a33d5ea22a574603a5a70f91513995759ed3
elliedotjpg <79950128+elliedotjpg@users.noreply.github.com>
2022-04-20 21:17:07 +0800

    🚧 Camera now follows player but FIX on awake!

--- Wanderfell/Assets/Scripts/CameraFollowPlayer.cs
+++ Wanderfell/Assets/Scripts/CameraFollowPlayer.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
+
     public float FollowSpeed = 2f;
     public Transform target;
+    public float yOffset = 1f;
 
+
+    // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
