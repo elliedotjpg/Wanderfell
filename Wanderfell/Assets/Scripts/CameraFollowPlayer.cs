0f8b752675598656f58cc41bb4f1f6a231f0653f
elliedotjpg <79950128+elliedotjpg@users.noreply.github.com>
2022-04-20 19:31:59 +0800

    🚧 CameraFollowPlayer script in progress!

---
+++ Wanderfell/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+}
