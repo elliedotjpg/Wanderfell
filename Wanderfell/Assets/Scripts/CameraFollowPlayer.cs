52f2111fb4be6778205b294a8dfd9b40d962535d
elliedotjpg <79950128+elliedotjpg@users.noreply.github.com>
2022-04-22 11:44:40 +0800

    🚧 FIX transition to next scene for player movement

--- Wanderfell/Assets/Scripts/CameraFollowPlayer.cs
+++ Wanderfell/Assets/Scripts/CameraFollowPlayer.cs
@@ -8,16 +8,21 @@ public class CameraFollowPlayer : MonoBehaviour
     public float FollowSpeed = 2f;
     public Transform target;
     public float yOffset = 1f;
+    public float xOffset = 1f;
+    
+    public float startPositionY = 1f;
+    public float startPositionX = 1f;
+
 
     void Start()
     {
-        transform.position = new Vector3(target.position.x, target.position.y + yOffset);
+        transform.position = new Vector3(startPositionX, startPositionY);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
