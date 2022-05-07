759a01820ecc19f051ca3077c36f24973221c665
elliedotjpg <79950128+elliedotjpg@users.noreply.github.com>
2022-05-07 22:30:07 +0800

    🍱 Updated asset library

---
+++ Wanderfell/Assets/Scripts/SpawnError.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnError : MonoBehaviour
+{
+    [SerializeField] private GameObject errorPopUpPrefab;
+
+    public float transformPositionX;
+    public float transformPositionY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
