768315f344d046d1d46968ff8bccb86e4986dd6f
elliedotjpg <79950128+elliedotjpg@users.noreply.github.com>
2022-05-07 22:32:07 +0800

    🔨 Added and updated PauseManager script

---
+++ Wanderfell/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PauseGameWithKey();
+    }
+
+    private void PauseGameWithKey()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (!pausePanel.activeSelf)
+            {
+                pausePanel.SetActive(true);
+                Debug.Log("Game PAUSED!");
+                Time.timeScale = 0f;
+            }
+            else //if (pausePanel != null)
+            {
+                pausePanel.SetActive(false);
+                Debug.Log("Game UNPAUSED!");
+                Time.timeScale = 1f;
+            }
+        }
+            
+    }
+
+    void PauseGameWithButton()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        Debug.Log("Game PAUSED!");
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+        Debug.Log("Game UNPAUSED!");
+ 
+    }
+}
